c8a36f8ec55bdddeaffbb449dda6e18ee7649b86
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-07-14 18:15:29 +0500

    order class

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 
 class Product(models.Model):
@@ -15,3 +16,6 @@ class UserProfile(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name}'s Profile"
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
